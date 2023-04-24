a983d1903567fae9cfc72b3a139070ada08cd2b9
Tania <taniaeunice277@gmail.com>
2023-04-23 20:32:39 -0700

    a

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,38 +1,24 @@
-import React from 'react';
-import { FaCloud, FaSun, FaSnowflake, FaCloudRain } from 'react-icons/fa';
-// import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-
-const WeatherCard = ({ city, weather, temp }) => {
-  const getWeatherIcon = () => {
-    switch (weather) {
-      case 'Clouds':
-        return <FaCloud />;
-      case 'Clear':
-        return <FaSun />;
-      case 'Snow':
-        return <FaSnowflake />;
-      case 'Rain':
-        return <FaCloudRain />;
-      default:
-        return <FaSun />;
-    }
+const [CitiesData, setCitiesData] = useState(0);
+  const [Data, setData] = useState([]);
+  const getData = async () => {
+    const city = cities[CitiesData].name;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a7e19e7c3d91c685e79c56b6f9d75ffb&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return { city, temperature: data.main.temp, feels_like: data.main.feels_like, humidity: data.main.humidity, pressure: data.main.pressure, wind: data.wind.speed, clouds: data.clouds.all, visibility: data.visibility };
   };
 
-  return (
-  <div className="col-lg-6 text-center col-sm-12 card mt-3 mx-4">
-    <div className='row'>
-        <div className='col-6 card-body'>
-            <h2 className='city text-center card-title'>{city}</h2>
-        </div>
-        <div className='col-6 card-text'>
-            <div className="weather-icon">{getWeatherIcon()}</div>
-            <div className="temp">{temp}°C</div>
-        </div>
-    </div>
-  </div>
-);
-};
+  const DataSyncShow = async () => { 
+    const newData = await getData();
+    setData([...Data, newData]); 
+    setCitiesData(CitiesData + 1);
+  };
 
-export default WeatherCard;
+  useEffect(() => {
+    onMessage(messaging, message => {
+      console.log("Tu mensaje: ", message);
+      toast(message.notification.title);
+    });
+    DataSyncShow();
+  }, [CitiesData]);
